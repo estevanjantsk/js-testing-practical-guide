2fcf32094ddc25683eb2e941dd2e82d5f7840a0c
Estevan Jantsk <estevan.jantsk@gmail.com>
2022-12-17 20:34:54 -0300

    add test suites with describe

--- basic-testing-frontend/src/util/validation.test.js
+++ basic-testing-frontend/src/util/validation.test.js
@@ -1,44 +1,48 @@
-import { it, expect } from "vitest";
+import { it, expect, describe } from "vitest";
 
 import { validateNumber, validateStringNotEmpty } from "./validation";
 
-it("should NOT throw an error if input is a number", () => {
-  // arrange
-  const input = 1;
-  // act
-  const resFn = () => {
-    validateNumber(input);
-  };
-  // assert
-  expect(resFn).not.toThrow();
+describe("validateNumber()", () => {
+  it("should NOT throw an error if input is a number", () => {
+    // arrange
+    const input = 1;
+    // act
+    const resFn = () => {
+      validateNumber(input);
+    };
+    // assert
+    expect(resFn).not.toThrow();
+  });
+  it("should throw an error if input is NOT a number", () => {
+    // arrange
+    const input = "not a number";
+    // act
+    const resFn = () => {
+      validateNumber(input);
+    };
+    // assert
+    expect(resFn).toThrow(/Invalid number input/);
+  });
 });
-it("should throw an error if input is NOT a number", () => {
-  // arrange
-  const input = "not a number";
-  // act
-  const resFn = () => {
-    validateNumber(input);
-  };
-  // assert
-  expect(resFn).toThrow(/Invalid number input/);
-});
-it("should NOT throw an error if input is a not empty string", () => {
-  // arrange
-  const input = "string";
-  // act
-  const resFn = () => {
-    validateStringNotEmpty(input);
-  };
-  // assert
-  expect(resFn).not.toThrow();
-});
-it("should throw an error if input an empty string", () => {
-  // arrange
-  const input = "";
-  // act
-  const resFn = () => {
-    validateStringNotEmpty(input);
-  };
-  // assert
-  expect(resFn).toThrow(/Invalid input - must not be empty/);
+describe("validateStringNotEmpty()", () => {
+  it("should NOT throw an error if input is a not empty string", () => {
+    // arrange
+    const input = "string";
+    // act
+    const resFn = () => {
+      validateStringNotEmpty(input);
+    };
+    // assert
+    expect(resFn).not.toThrow();
+  });
+  it("should throw an error if input an empty string", () => {
+    // arrange
+    const input = "";
+    // act
+    const resFn = () => {
+      validateStringNotEmpty(input);
+    };
+    // assert
+    expect(resFn).toThrow(/Invalid input - must not be empty/);
+  });
 });
