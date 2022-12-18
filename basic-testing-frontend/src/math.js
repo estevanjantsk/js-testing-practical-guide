e94d7c7db5c80b59d6b142d1d71251145f50139f
Estevan Jantsk <estevan.jantsk@gmail.com>
2022-12-18 10:48:07 -0300

    refactoring code

--- basic-testing-frontend/src/math.js
+++ basic-testing-frontend/src/math.js
@@ -1,3 +1,5 @@
+import { cleanNumbers } from "./util/numbers.js";
+
 export function add(numbers) {
   let sum = 0;
 
@@ -5,4 +7,16 @@ export function add(numbers) {
     sum += +number;
   }
   return sum;
+}
+
+export function calculateResult(numberValues) {
+  let result = "";
+  try {
+    const numbers = cleanNumbers(numberValues);
+
+    result = add(numbers).toString();
+  } catch (error) {
+    result = error.message;
+  }
+  return result;
 }
