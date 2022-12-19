37c377d295b70c320e0025a73fdae800cbe0b5aa
Estevan Jantsk <estevan.jantsk@gmail.com>
2022-12-19 08:55:40 -0300

    mocks

--- mocks-spies/src/util/io.test.js
+++ mocks-spies/src/util/io.test.js
@@ -1,7 +1,37 @@
-import { describe, it, expect } from "vitest";
+import { it, expect, vi } from "vitest";
+import { promises as fs } from "fs";
 
 import { writeData } from "./io";
 
-describe("writeData()", () => {
-  it("", () => {});
+vi.mock("fs");
+vi.mock("path", () => {
+  return {
+    default: {
+      join: (...args) => {
+        return args[args.length - 1];
+      },
+    },
+  };
+});
+
+it("should execute the writeFile method", () => {
+  const testData = "Test";
+  const testFilename = "test.txt";
+
+  writeData(testData, testFilename);
+
+  // return expect(writeData(testData, testFilename)).resolves.toBeUndefined();
+  expect(fs.writeFile).toBeCalled();
+  expect(fs.writeFile).toBeCalledWith(testFilename, testData);
+});
+
+it("should return a promise that resolves to no value if called correctly", () => {
+  const testData = "Test";
+  const testFilename = "test.txt";
+
+  writeData(testData, testFilename);
+
+  // return expect(writeData(testData, testFilename)).resolves.toBeUndefined();
+  // expect(fs.writeFile).toBeCalled();
+  // expect(fs.writeFile).toBeCalledWith(testFilename, testData);
 });
